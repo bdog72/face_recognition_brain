81828976e1389a7b499dd28a45b9dda6982ac0f6
Brian Bycynski <high5bri@hotmail.com>
2018-01-26 13:28:40 -0500

    still working

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,7 +4,7 @@ const Register = ({ onRouteChange }) => {
     return (
         <article class="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
-                <form className="measure-center">
+                <div className="measure-center">
                     <fieldset
                         id="sign_up"
                         className="ba b--transparent ph0 mh0"
@@ -58,7 +58,7 @@ const Register = ({ onRouteChange }) => {
                             value="Register"
                         />
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     )
